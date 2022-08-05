a2f58c57e6895eaf981354ab2ea55482870cc3b1
Lo0syash <arturdzhan2004@gmail.com>
2022-08-05 23:41:44 +0300

    add button copy and remove checkbox

--- WpfApp4/WpfApp4/DB.Designer.cs
+++ WpfApp4/WpfApp4/DB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\artur\source\repos\WpfApp4\WpfApp4\DB.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Users\artur\OneDrive\Documents\GitHub\transport-rental\WpfApp4\WpfApp4\DB.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
